89043833d3fb4d19c2c431a16fa8ddeabc06ce3e
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-05-18 17:39:41 -0300

    Requisito 9

--- src/challenges.js
+++ src/challenges.js
@@ -89,11 +89,43 @@ function fizzBuzz(numbers) {
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(string) {
+  let result = "";
+  for(let index = 0; index < string.length; index += 1){
+    if(string[index] === "a"){
+      result += "1";
+    }else if(string[index] === "e"){
+      result += "2";
+    }else if(string[index] === "i"){
+      result += "3";
+    }else if(string[index] === "o"){
+      result += "4";
+    }else if(string[index] === "u"){
+      result += "5";
+    }else{
+      result += string[index];
+    }
+  }
+  return result;
 }
-function decode() {
-  // seu código aqui
+function decode(string) {
+  let result = "";
+  for(let index = 0; index < string.length; index += 1){
+    if(string[index] === "1"){
+      result += "a";
+    }else if(string[index] === "2"){
+      result += "e";
+    }else if(string[index] === "3"){
+      result += "i";
+    }else if(string[index] === "4"){
+      result += "o";
+    }else if(string[index] === "5"){
+      result += "u";
+    }else{
+      result += string[index];
+    }
+  }
+  return result;
 }
 
 // Desafio 10
